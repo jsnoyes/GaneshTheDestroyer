6076a62d7a294faf842cadcb14336644cf0e6413
jsnoyes <jacno0000@gmail.com>
2021-09-25 17:22:35 -0500

    Override equals and get hash code on point.

--- Starter.Core/Point.cs
+++ Starter.Core/Point.cs
@@ -10,5 +10,28 @@
             X = x;
             Y = y;
         }
+
+   public override bool Equals(Object obj)
+   {
+      //Check for null and compare run-time types.
+      if ((obj == null) || ! this.GetType().Equals(obj.GetType()))
+      {
+         return false;
+      }
+      else {
+         Point p = (Point) obj;
+         return (X == p.X) && (Y == p.Y);
+      }
+   }
+
+   public override int GetHashCode()
+   {
+      return (X << 2) ^ Y;
+   }
+
+    public override string ToString()
+    {
+        return String.Format("Point({0}, {1})", X, Y);
+    }
     }
 }
