b60bd72ee23095ed6d538410d48efb03cac90e2f
jsnoyes <jacno0000@gmail.com>
2021-10-09 19:49:36 -0500

    Update SnakeController.cs

--- Starter.Api/Controllers/SnakeController.cs
+++ Starter.Api/Controllers/SnakeController.cs
@@ -136,7 +136,10 @@ namespace Starter.Api.Controllers
                 {
                     var dst = GetDistanceToClosestRequestedPoints(gameStatusRequest, snakeHS, occupied, neighbor, closestNeighborToHeadDistance);
  Console.WriteLine(dst.ToString() + " " + closestNeighborToHeadDistance);
-                    if(dst < closestNeighborToHeadDistance)
+                    var neighborsNeighbors = GetOpenNeighbors(gameStatusRequest, occupied, neighbor);
+                    var otherSnakeHead = otherSnakes.First().Head;
+                    var otherSnakeHeadIsNeigbor = (Math.Abs(Math.Abs(otherSnakeHead.X) - Math.Abs(you.Head.X)) +  Math.Abs(Math.Abs(otherSnakeHead.X) - Math.Abs(you.Head.X))) == 1;
+                    if(dst < closestNeighborToHeadDistance && neighborsNeighbors.Count() > (otherSnakeHeadIsNeigbor ? 0 : 1)) // don't go thru a 1 spot opening unless the other snakes head is next to it. 
                     {
                         closestNeighborToHeadDistance = dst;
                         closestNeighborToHead = neighbor;
